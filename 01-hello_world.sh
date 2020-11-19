9c6141c39805d5bfd6a0e715f5f3efeec8fa60c6
Chris <lol_cigy_lol@hotmail.com>
2020-11-19 15:32:45 -0500

    5 dolla sucky sucky

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# ceci est un commentaire
+
+echo "salut"
+# ou
+echo salut
+# ou
+echo 'salut'
+
+echo -n "Vive "
+echo "Linux"
+# va ecrire Vive Linux sans saut de ligne
+
+echo -e "1\n2\n3\n4" #-e interprete les metacaracteres
+
+read -p "My name is?" nom #read = cin, -p "mon message" >> variable
+
+echo "tchic ka thcick la tchic" $nom
+
